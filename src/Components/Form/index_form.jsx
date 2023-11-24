7a7e89494926ee9d341e05c60b2abfcc2772f4ec
Alimurz <148546039+Alimurz@users.noreply.github.com>
2023-11-24 19:23:09 +0600

    Update index_form.jsx

--- src/Components/Form/index_form.jsx
+++ src/Components/Form/index_form.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 import styles from './styles.module.css'
 function Form() {
-    return(
+    return(import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+function Form({ addMeme }) {
+  const [title, setTitle] = useState('');
+
+  const addToList = () => {
+    const newMeme = {
+      id: Math.random(),
+      title: title,
+      body: ''
+    };
+    addMeme(newMeme);
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          className={styles.input_text}
+          placeholder="Write the title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button className={styles.form_btn} onClick={addToList}>
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
  <form>
     <input type="text" className={styles.input_text} placeholder="write the title"/>
     <button className={styles.form_btn}>add</button>
