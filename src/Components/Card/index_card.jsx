35aa92eb0e84480589af7cebe13e79e2d57b7a82
Alimurz <148546039+Alimurz@users.noreply.github.com>
2023-11-24 19:23:29 +0600

    Update index_card.jsx

--- src/Components/Card/index_card.jsx
+++ src/Components/Card/index_card.jsx
@@ -1,35 +1,28 @@
-import Styles from "./styles.module.css"
-import React from 'react'
-import { useState } from "react";
-
-   
-
+import React, { useState } from 'react';
+import Styles from "./styles.module.css";
 
 function Card(props) {
-    const [showMeme, setShowMeme] = useState(true);
-    const hideMeme = () => {
-        setShowMeme(!showMeme);
-      }
+  const [showMeme, setShowMeme] = useState(true);
+
+  const hideMeme = () => {
+    setShowMeme(false);
+  };
+
   return (
-    
     <div className={Styles.container}>
-      <div className={Styles.card}>
-        {showMeme && (
+      {showMeme && (
+        <div className={Styles.card}>
           <div className={Styles.card_item}>
             <img src={props.image} alt="." className={Styles.card_img} />
             <span className={Styles.card_name}>{props.name}</span>
             <span className={Styles.card_title}>{props.title}</span>
             <span className={Styles.card_body}>{props.body}</span>
-            
           </div>
-        )}
-        <div className={Styles.card_item}>
-        <button onClick={hideMeme}>
-              SHOW {showMeme}
-            </button>
+          <div className={Styles.card_item}>
+            <button onClick={hideMeme}>Remove</button>
+          </div>
         </div>
-        
-      </div>
+      )}
     </div>
   );
 }
